09dfef039acd1641a7c42427e6279a81a115fd03
catalin-burcea <burcea.iulian.catalin@gmail.com>
2014-07-02 10:18:30 +0300

    show list of employees

--- absence_management.web/src/com/crispico/absence_management/controller/EmployeeTableController.java
+++ absence_management.web/src/com/crispico/absence_management/controller/EmployeeTableController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.crispico.absence_management.HibernateUtil;
+import com.crispico.absence_management.dao.EmployeeHibernateDao;
 import com.crispico.absence_management.model.Employee;
 
 /**
@@ -19,13 +21,10 @@ public class EmployeeTableController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		List<Employee> list = new ArrayList<>();
-		for (int i = 0; i < 15; i++) {
-			Employee employee = new Employee();
-			employee.setFirstName("First " + i);
-			employee.setLastName("Last " + i);
-			list.add(employee);
-		}
+
+		EmployeeHibernateDao ehd = new EmployeeHibernateDao();
+		List<Employee> list = ehd.getAll();
+		
 		req.setAttribute("list", list);
 		req.getRequestDispatcher("views/employeeTable.jsp").forward(req, resp);
 	}
